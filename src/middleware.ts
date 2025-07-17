6b327da1e04587ea3fb5bad959f294ac139ef802
Burhan <burhanchughtai744@gmail.com>
2025-07-17 12:35:40 +0500

    'updated-code'

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ export function middleware(request: NextRequest) {
     return new NextResponse(null, {
       status: 200,
       headers: {
-        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Origin": "https://prescripto-backend-eight-red.vercel.app",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
         "Access-Control-Allow-Headers": "Content-Type, Authorization, atoken",
       },
